4ef42599d4b7cf392597cdebedc4a8d7fc2a5ee1
Jon Harmon <jonthegeek@gmail.com>
2026-09-21 06:05:54 -0500

    Tweaks (see thread on DSLC for breakdown).

--- data/curated/new_submission/cleaning.R
+++ data/curated/new_submission/cleaning.R
@@ -1,29 +1,20 @@
-# Paste code used to create the curated dataset here. Include comments as
-# necessary. If you did not need to clean the data, use a comment like the one
-# below, but also load the data with readr::read_csv() to ensure the data can be
-# loaded, and to use with `saving.R`. Delete this block of comments.
-
-# Clean data provided by <source of data>. No cleaning was necessary.
-dataset <- readr::read_csv("dataset_url")
-
-# The original dataset comes from the UN-Habitat website 
-# https://data.unhabitat.org/pages/open-spaces-and-green-areas
-# So download the second file on the website
-# the .xlsx file is called 
-# 'Urban_green_area_proportion_and_green_area_per_capita.xlsx'
-
 # Packages 
-library(tidytuesdayR)
 library(readxl)
 library(janitor)
 library(dplyr)
 library(tidyr)
+library(withr)
+
+# Download the dataset from the UN-Habitat website and save it as a local file.
+xlsx_url <- "https://guo-un-habitat.maps.arcgis.com/sharing/rest/content/items/4350427c2e3341f69de14cb00c8d3ef3/data"
+xlsx_file <- withr::local_tempfile(fileext = ".xlsx")
+download.file(xlsx_url, xlsx_file, mode = "wb")
 
 # Clean dataset 
 # Ignore the first sheet because it has metadata and info 
 # Then we will fix the columns that are in wide format and leave 
 # the entire dataset in long format 
-urban <- readxl::read_excel('tt_submission/Urban_green_area_proportion_and_green_area_per_capita.xlsx', 
+urban <- readxl::read_excel(xlsx_file, 
                             sheet = 2, 
                             na = '-') |> 
   janitor::clean_names(case = "small_camel") |> 
@@ -37,9 +28,3 @@ urban <- readxl::read_excel('tt_submission/Urban_green_area_proportion_and_green
     greenAreaPerCapitaM2 = greenAreaPerCapita
   ) |>
   mutate(year = as.integer(year))
-
-# tt_save_dataset(urban)
-
-
-
-
